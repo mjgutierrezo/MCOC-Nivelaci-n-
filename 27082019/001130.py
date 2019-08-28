9ff7674cb140a7f9dd5b61997575f30ba99f3ed6
mjgutierrezo <53712876+mjgutierrezo@users.noreply.github.com>
2019-08-28 00:03:08 -0400

    Add files via upload

---
+++ 27082019/001130.py
@@ -0,0 +1,15 @@
+# Nivelación MCOC, "Numpy"
+"""
+Librería numérica en Python
+#001130
+"""
+#se pueden mezclar los arreglos
+
+a_array = np.array([1, 2, 3, 4, 5])
+b_array = np.array([6, 7, 8, 9, 10])
+
+print (a_array + b_array) #retorna suma entre cada índice de los arreglos
+print (a_array +30) #se suma 30 a cada índice
+print (a_array * 10) #se multiplica cada índice
+
+#plt.imshow(photo[:, :, 0].T) se traspone arreglo de foto
